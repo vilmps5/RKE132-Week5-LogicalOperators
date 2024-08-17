6d4a19ca9ab09dbd274d0d796f80c5ec05e2b173
Vilmar Viimsalu <vilmar.viimsalu@tktk.ee>
2024-08-17 19:17:13 +0300

    week 5

---
+++ OROperator/Program.cs
@@ -0,0 +1,23 @@
+﻿//Math >= 90 OR Chemistry >= 90
+
+//math && chemistry || math && biology || Chemistry && biology
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your Math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology result:");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemisty result:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if ((math >= 90 && chemistry >= 90) || (math >= 90 && biology >= 90) || (chemistry >= 90 && biology >= 90))
+{
+    Console.WriteLine("Congratulations! You got accepted");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved.");
+}
